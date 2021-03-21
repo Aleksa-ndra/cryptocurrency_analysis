db4042e268cd7986595ed64380b86a8e5fb9991b
Aleksa-ndra <81102925+Aleksa-ndra@users.noreply.github.com>
2021-03-21 17:51:50 +0100

    First commit
    File includes below steps:
    -upload BitBey API
    -display URL
    -display json file content

---
+++ First commit..py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import json
+import requests
+
+
+# In[4]:
+
+
+req = requests.get("https://bitbay.net/API/Public/BTCPLN/orderbook.json")
+req.status_code
+
+
+# In[6]:
+
+
+print(req.url)
+
+
+# In[7]:
+
+
+req.text
+
+
+# In[ ]:
+
+
+
+
